7ae7f087503266b4b0e3e6a50f8284cb96768f55
shrenik49 <sutarshrenik3@gmail.com>
2023-11-15 18:53:30 +0530

    Updated completed code

--- Contact/main.py
+++ Contact/main.py
@@ -1,14 +1,41 @@
 contacts = {}
 
 def add_contact():
-    name = input("Enter name")
-    number = input("Enter mobile number")
-    contacts[name]={number}
-
+    name = input("Enter name: ")
+    number = input("Enter mobile number: ")
+    contacts[name]=number
+    print("Added contact for {} successfully".format(name))
 
 def view_all_contacts():
-    for name ,number in contacts.items():
-        print(f"{name} : {number}" )
+    if len(contacts)==0:
+        print("No contact to show .Please add contacts")
+    else:
+        for name ,number in contacts.items():
+            print("{} : {}".format(name,number))
+
+def delete_contact():
+    name = input("Enter name to delete: ")
+    try:
+        del contacts[name]
+        print("Deleted contact for {} successfully".format(name))
+    except KeyError:
+        print("Please enter valid name to delete.")
+ 
+def update_contact():
+    name = input("Enter name to update contact: ")
+    newNumber = input("Enter new number for contact with {}.".format(name))
+    if contacts[name] :
+        contacts[name]=newNumber
+        print("Updated contact for {} successfully".format(name))
+    else:
+        print("Contact not available for this name")
+        
+def search_contact():
+    name = input("Enter name to search contact: ")
+    if contacts[name] :
+        print("{}:{}".format(name,contacts[name]))
+    else:
+        print("Contact not available for this name.")
 
 while True:
     print("\nMenu:")
@@ -18,20 +45,17 @@ while True:
     print("4. View all contacts")
     print("5. Search contact by name")
     print("6. Exit")
-
     choice = input("Enter your choice: ")
-
     if choice == '1':
         add_contact()
-
     elif choice == '4':
         view_all_contacts()
-    # elif choice == '2':
-    #     delete_contact()
-    # elif choice == '3':
-    #     update_contact()
-    # elif choice == '5':
-    #     search_contact()
+    elif choice == '2':
+        delete_contact()
+    elif choice == '3':
+        update_contact()
+    elif choice == '5':
+        search_contact()
     elif choice == '6':
         print("Exiting the program. Goodbye!")
         break
