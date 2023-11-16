b104b8a4e1f1a5f54b4f644a2f10179872f69d74
shrenik49 <sutarshrenik3@gmail.com>
2023-11-16 14:04:00 +0530

    fixed colors and serch by number function

--- Contact/main.py
+++ Contact/main.py
@@ -10,44 +10,65 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 contacts = {}
-
 def add_contact():
-    name = input("Enter name: ")
-    number = input("Enter mobile number: ")
-    contacts[name]=number
-    print(bcolors.OKGREEN + "Added contact for {} successfully!".format(name)+ bcolors.ENDC)
+    name = input(bcolors.BOLD+"Enter name: "+bcolors.ENDC)
+    number = input(bcolors.BOLD+"Enter mobile number: "+bcolors.ENDC)
+    if(len(number)==10):
+        contacts[name]=number
+        print(bcolors.OKGREEN + "Added contact for {} successfully!".format(name)+ bcolors.ENDC)
+    else:
+        print(bcolors.WARNING + "Please enter valid 10 digit number"+bcolors.ENDC)
 
 def view_all_contacts():
     if len(contacts)==0:
-        print("No contact to show. Please add contacts!")
+        print(bcolors.WARNING+"No contact to show. Please add contacts!"+bcolors.ENDC)
     else:
-        print("Contact List:")
+        print(bcolors.BOLD+"Contact List:"+bcolors.ENDC)
         for name ,number in contacts.items():
-            print("{} : {}".format(name,number))
-        print("Total {} contact(s)...".format(len(contacts)))
+            print(bcolors.OKBLUE+"{} : {}".format(name,number)+ bcolors.ENDC)
+        print(bcolors.UNDERLINE+"Total {} contact(s)...".format(len(contacts))+bcolors.ENDC)
+
 def delete_contact():
-    name = input("Enter name to delete: ")
+    name = input(bcolors.BOLD+"Enter name to delete: "+bcolors.ENDC)
     try:
         del contacts[name]
-        print("Deleted contact for {} successfully!".format(name))
+        print(bcolors.WARNING+"Deleted contact for {} successfully!".format(name)+bcolors.ENDC)
     except KeyError:
-        print("Please enter valid name to delete.")
+        print(bcolors.WARNING+"Please enter valid name to delete."+bcolors.ENDC)
  
 def update_contact():
-    name = input("Enter name to update contact: ")
-    newNumber = input("Enter new number for contact with {}.".format(name))
-    if contacts[name] :
-        contacts[name]=newNumber
-        print("Updated contact for {} successfully!".format(name))
+    name = input(bcolors.BOLD+"Enter name to update contact: "+bcolors.ENDC)
+    newNumber = input(bcolors.BOLD+"Enter new number for contact with {}.".format(name)+bcolors.ENDC)
+    if name in contacts :
+        if len(newNumber)== 10:
+            contacts[name]=newNumber
+            print(bcolors.OKBLUE+"Updated contact for {} successfully!".format(name)+bcolors.ENDC)
+        else:
+            print(bcolors.WARNING + "Please enter valid 10 digit number"+bcolors.ENDC)
     else:
-        print("Contact not available for this name.")
+        print(bcolors.WARNING+"Contact not available for this name."+bcolors.ENDC)
         
 def search_contact():
-    name = input("Enter name to search contact: ")
-    if contacts[name] :
-        print("{}:{}".format(name,contacts[name]))
+    if len(contacts)==0:
+        print(bcolors.WARNING+"No contact to show. Please add contacts!"+bcolors.ENDC)
     else:
-        print("Contact not available for this name.")
+        name = input(bcolors.BOLD+"Enter name to search contact: "+bcolors.ENDC)
+        if name in contacts:
+            print(bcolors.OKBLUE+"{}:{}".format(name,contacts[name])+bcolors.ENDC)
+        else:
+            print(bcolors.WARNING+"Contact not available for this name."+bcolors.ENDC)
+
+def search_contact_bynumber():
+    number = input(bcolors.BOLD+"Enter number to search contact: "+bcolors.ENDC)
+    flag=0
+    for name,phone in contacts.items():
+        if number == phone:
+            print(bcolors.OKBLUE+"{}:{}".format(name,phone)+bcolors.ENDC)
+            flag=1
+            break
+    if flag==0:
+        print(bcolors.WARNING+"Contact not available for this name."+bcolors.ENDC)
+        
 
 while True:
     print("\nMenu:")
@@ -56,20 +77,23 @@ while True:
     print("3. Update contact")
     print("4. View all contacts")
     print("5. Search contact by name")
-    print("6. Exit")
+    print("6. Search contact by number")
+    print("7. Exit")
     choice = input("Enter your choice: ")
     print("--------------- Output ---------------")
     if choice == '1':
         add_contact()
-    elif choice == '4':
-        view_all_contacts()
     elif choice == '2':
         delete_contact()
     elif choice == '3':
         update_contact()
+    elif choice == '4':
+        view_all_contacts()
     elif choice == '5':
         search_contact()
     elif choice == '6':
+        search_contact_bynumber()
+    elif choice == '7':
         print("Exiting the program. Goodbye!")
         break
     else:
